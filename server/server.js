6502ce513a82a06dab428c5176d22e6e0b757710
SotaNoniwa <surfersou@gmail.com>
2023-08-15 02:23:54 +0900

    Step 7: Save a new note with mongoose

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
 import express from "express";
 import dotenv from "dotenv/config";
 import mongoose from "mongoose";
+import Note from "./models/noteModel.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
     return res.status(200).send("Welcome to MERN Stack Tutorial!");
 });
 
-const PORT = process.env.PORT;
+// Route to save a new note
+app.post("/notes", async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        const note = await Note.create({ title, content });
+
+        return res.send(note);
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 mongoose
     .connect(process.env.MONGO_DB_URL)
