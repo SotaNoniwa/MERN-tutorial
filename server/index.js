d9d04f276bf27982c925494fae5501606883628a
SotaNoniwa <surfersou@gmail.com>
2023-08-14 15:02:02 +0900

    Step 4: Create the first HTTP route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv/config";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    return res.status(200).send("Welcome to MERN Stack Tutorial!");
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`App is listening to port ${PORT}`);
