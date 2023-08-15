aa9a3eafe046da488b0d82ad9db76542c664a307
SotaNoniwa <surfersou@gmail.com>
2023-08-15 19:34:54 +0900

    Step 13: CORS policy in node and express

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import dotenv from "dotenv/config";
 import mongoose from "mongoose";
 import Routes from "./routes/noteRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS policy
+// Allow all origins (domains) with default of cors(*)
+app.use(cors());
 
 app.use("/notes", Routes);
 
